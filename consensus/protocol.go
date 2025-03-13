eb91c3c5c9d3d3e7595aa9ea8127c250966ac7d0
H-Hurrik <xinjiehuang246@gmail.com>
2025-03-13 09:00:46 +0000

    protocol

---
+++ consensus/protocol.go
@@ -0,0 +1,5 @@
+package consensus
+
+const (
+	pbft = "/pbft/1.0.0" // 自定义协议标识符
+)
--- core/blockchain.go
+++ core/blockchain.go
@@ -6,3 +6,11 @@ type Blockchain struct {
 	db     *storage.LevelDB
 	TxPool *TxPool
 }
+
+func NewBlockchain() *Blockchain {
+	return &Blockchain{}
+}
+
+func NewGenesisBlock() *Block {
+	return NewBlock(nil, nil)
+}
