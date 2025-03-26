71eefaec58c3ee4e7b2cbd8833f7a8d4ff6a39a8
Abenr6637 <824752548@qq.com>
2025-03-26 04:55:44 +0000

    add test script

--- README.md
+++ README.md
@@ -19,3 +19,43 @@ networks
 cli
 
 utils
+
+
+
+## tmux操作:
+
+* **查看列表**
+```bash
+tmux ls
+```
+输出:
+```
+node1: 1 windows (created Mon Mar 25 10:00:00 2024)
+node2: 1 windows (created Mon Mar 25 10:00:05 2024)
+```
+
+* **进入终端**
+```bash
+tmux attach-session -t node1
+# 或简写:
+tmux a -t node1
+```
+
+* **退出而不终止**
+```
+Ctrl + B，然后按 D
+```
+
+* **退出且终止**
+```
+Ctrl + C
+```
+
+* **关闭节点**
+```
+tmux kill-session -t node1
+tmux kill-session -t node2
+
+# 关闭所有会话
+tmux kill-server
+```
--- test.sh
+++ test.sh
@@ -1,35 +1,64 @@
 #!/bin/bash
 
-# 终端颜色
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
+# 搭建三个节点的区块链系统
 
-# 删除旧的 multiaddr 文件
+# 删除旧的 block_storage 文件
 rm -rf ./block_storage
 mkdir -p ./block_storage/
 
-echo -e "${GREEN}启动第一个节点...${NC}"
-go run main.go --db ./block_storage/node1_data --consensus pow
-# go run main.go --db ./block_storage/node2_data --consensus pow
+# 设置数据库目录
+NODE1_DB="./block_storage/node1_data"
+NODE2_DB="./block_storage/node2_data"
+NODE3_DB="./block_storage/node3_data"
 
-# 等待节点启动
-sleep 3
+NODE1_LISTEN="/ip4/127.0.0.1/tcp/50001"
+NODE2_LISTEN="/ip4/127.0.0.1/tcp/50002"
+NODE3_LISTEN="/ip4/127.0.0.1/tcp/50003"
 
-# # 提取第一个节点的 Multiaddr
-# NODE1_ADDR=$(grep -oE '/ip4[^ ]+' ./block_storage/node1_addr.txt | head -n 1)
-# if [ -z "$NODE1_ADDR" ]; then
-#   echo "未能获取 Node 1 的地址"
-#   exit 1
-# fi
-# echo -e "${GREEN}Node 1 地址: ${NODE1_ADDR}${NC}"
+NODE1_LOG="./block_storage/node1.log"
+NODE2_LOG="./block_storage/node2.log"
+NODE3_LOG="./block_storage/node3.log"
 
-# echo -e "${GREEN}启动第二个节点...${NC}"
-# gnome-terminal -- bash -c "go run main.go --db ./block_storage/node2_data | tee ./block_storage/node2_addr.txt"
 
-# # 等待第二个节点启动
-# sleep 5
+# 启动第一个节点，并将其输出重定向到 log 文件
+tmux new-session -d -s node1 "go run main.go --db $NODE1_DB --listen $NODE1_LISTEN --consensus pow | tee $NODE1_LOG"
 
-# echo -e "${GREEN}Node 2 连接到 Node 1...${NC}"
-# echo "connect $NODE1_ADDR" | nc -w 1 localhost 9001
+# 等待一会儿，确保节点1启动完成
+sleep 5
 
-# echo -e "${GREEN}两个节点已成功启动并连接!${NC}"
+# 解析节点1的 p2p 地址
+NODE1_PEER_ID=$(grep -oP 'ID:\s+\K[a-zA-Z0-9]+' $NODE1_LOG | tail -1)
+
+NODE1_ADDR="$NODE1_LISTEN/p2p/$NODE1_PEER_ID"
+
+echo "Node 1 Address: $NODE1_ADDR"
+
+# 启动第二个节点
+tmux new-session -d -s node2 "go run main.go --db $NODE2_DB --listen $NODE2_LISTEN --consensus pow | tee $NODE2_LOG"
+
+sleep 5
+
+NODE2_PEER_ID=$(grep -oP 'ID:\s+\K[a-zA-Z0-9]+' $NODE2_LOG | tail -1)
+
+NODE2_ADDR="$NODE2_LISTEN/p2p/$NODE2_PEER_ID"
+
+echo "Node 2 Address: $NODE2_ADDR"
+
+# 启动第三个节点
+tmux new-session -d -s node3 "go run main.go --db $NODE3_DB --listen $NODE3_LISTEN --consensus pow | tee $NODE3_LOG"
+
+sleep 5
+
+NODE3_PEER_ID=$(grep -oP 'ID:\s+\K[a-zA-Z0-9]+' $NODE3_LOG | tail -1)
+
+NODE3_ADDR="$NODE3_LISTEN/p2p/$NODE3_PEER_ID"
+
+echo "Node 3 Address: $NODE3_ADDR"
+
+# 节点连接
+tmux send-keys -t node1 "connect $NODE2_ADDR" Enter
+tmux send-keys -t node2 "connect $NODE3_ADDR" Enter
+tmux send-keys -t node3 "connect $NODE1_ADDR" Enter
+
+echo "Blockchain Start!"
+
